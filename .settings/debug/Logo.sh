cdecf3461111fd89a687992722c20cbf42b7b43c
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2023-12-01 01:45:41 +0530

    Add files via upload

---
+++ .settings/debug/Logo.sh
@@ -0,0 +1,16 @@
+echo '
+[+]
+|       _   __            __                                     
+|     / | / /___  ____  / /_          _________ __   ___   ____  __
+|    /  |/ / __ \/ __ \/ __ \     / ___/ __ `/ | / / | / / / / /
+|  / /|  / /_/ / /_/ / /_/ /    (__  ) /_/ /| |/ /| |/ / /_/ / 
+| /_/ |_/\____/\____/_.___/____/____/\__,_/ |___/ |___/\__, /  
+|                       /_____/                      /____/    v2.7
+                        
+[+] Title    : AllHackingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
+[+] Github   : https://github.com/noob-savvy  -  nsi⁣ble responsible for any consequences of reading it. 
+[+] Coded By : Noob savvy  -  All materials are provided for educational purposes only! 
+[+]———————-——-–————————-—-–———-——————-—–————-————-—–———————-——–—————–-——[+]'|lolcat -p 1.6 -a -d 1 -s 30.0
+
+
+
