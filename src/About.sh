7b03e07fbcea4d4518254d1af490e71a8189c38c
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2023-11-30 23:50:20 +0530

    Add files via upload

---
+++ src/About.sh
@@ -0,0 +1,20 @@
+echo '
+    ┌──────────────────────────────────────────────────────────────┐
+    │  	           ┏━┓┏┓ ┏━┓╻ ╻╺┳╸	              │
+    │  		       ┣━┫┣┻┓┃ ┃┃ ┃ ┃ 		      │
+    │  		       ╹ ╹┗━┛┗━┛┗━┛ ╹ 		      │
+    │──────────────────────────────────────────────────────────────│
+    │                                                              │
+    │ >>   Hi, here you can see my telegram and github there       |
+    |       will be questions you will write in telegram.          │
+    │                                                              │
+    │ >>  Author   :  NOOB_SAVVY                               │
+    │     Github   :  https://github.com/noob-savvy               │
+    │     Telegram :   @noob_savvy              │
+    |     Info     :  Trank for installing AllHackingTools         |
+    │                                                              │
+    └──────────────────────────────────────────────────────────────┘ 
+'|lolcat -a -d 10 -p 1.5 -s 40.0
+cd && cd AllHackingTools  
+python3 src/Timer1.py
+python2 src/aboutMenu.py
