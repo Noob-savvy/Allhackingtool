80b34c4a83639d8c87855207b060a5a71350f7c3
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2023-12-01 01:50:43 +0530

    Add files via upload

---
+++ .settings/desing2/Logo.sh
@@ -0,0 +1,15 @@
+echo '
+[+]
+|       _   __            __                                     
+|     / | / /___  ____  / /_          _________ __   ___   ____  __
+|    /  |/ / __ \/ __ \/ __ \     / ___/ __ `/ | / / | / / / / /
+|  / /|  / /_/ / /_/ / /_/ /    (__  ) /_/ /| |/ /| |/ / /_/ / 
+| /_/ |_/\____/\____/_.___/____/____/\__,_/ |___/ |___/\__, /  
+|                       /_____/                      /____/    v2.7
+                        
+[+] Title    : AllHackingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
+[+] Github   : https://github.com/noob-savvy  -  nsi⁣ble responsible for any consequences of reading it. 
+[+] Coded By : Noob savvy  -  All materials are provided for educational purposes only! 
+
+{o}——————-——-–—————-—-–—————-————-—–————-———-—–—————————-——–—————–-——{+}
+'|lolcat -p 2.6 -a -d 2 -s 20.0
